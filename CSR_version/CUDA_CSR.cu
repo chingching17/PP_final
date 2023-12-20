84bac33dac6f7b035aeb8924cd13b68ebb5970c3
chingching17 <alicia.cs08@nctu.edu.tw>
2023-12-20 22:03:54 +0800

    csr ver done, haven't see whether correct

--- CSR_version/CUDA_CSR.cu
+++ CSR_version/CUDA_CSR.cu
@@ -266,18 +266,58 @@ void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr, int *A_size, int *IA
 
 }
 
+int matrix_multiply_cpu(const int n, const int m, const int l, const int A_size, const int IA_size, const int JA_size,
+                     const int *A, const int *IA, const int *JA, const int *b_mat, int *d_cc){
+    int *result_mat = new int[n * l];
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < l; ++j) {
+            result_mat[i * l + j] = 0;
+        }
+    }
+
+    for(int i=1; i<IA_size; i++){
+        for(int j=IA[i-1]; j<IA[i]; j++){
+            result_mat[i-1]+=A[j]*b_mat[JA[j]];
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < l; ++j) {
+            printf("%d ",result_mat[i * l + j]);
+        }
+        printf("\n");
+    }
+    cout << "okay" << endl;
+    int all_ok = 1;
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     for (int j = 0; j < l; ++j)
+    //     {
+    //         //printf("[%d][%d]:%d == [%d][%d]:%d, ", i, j, d_cc[i*k + j], i, j, d_c[i*k + j]);
+    //         if(result_mat[i*l + j] != d_cc[i*l + j])
+    //         {
+    //             all_ok = 0;
+    //         }
+    //     }
+    //     //printf("\n");
+    // }
+    delete[] result_mat;
+    return all_ok;
+}
+
 int main(int argc, char const *argv[])
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     int num;
     cin >> num;
     for(int i = 0; i < num; i++){
         /* Fixed seed for illustration */
         srand(3333);
-        int n, m, l, A_size, IA_size, JA_size;
+        int m, n, k, A_size, IA_size, JA_size;
         int *A, *IA, *JA, *b_mat;
-        construct_matrices(&n, &m, &l, &A_size, &IA_size, &JA_size, &A, &IA, &JA, &b_mat);
-        cout << "construct ok" << endl;
-        cout << "A_size" << A_size << endl;
+        construct_matrices(&m, &n, &k, &A_size, &IA_size, &JA_size, &A, &IA, &JA, &b_mat);
 
         // allocate memory in host RAM, h_cc is used to store CPU result
         int *h_b, *h_c, *h_cc, *h_A, *h_JA, *h_IA;
@@ -288,10 +328,10 @@ int main(int argc, char const *argv[])
         cudaMalloc((void **) &h_IA, sizeof(int)* IA_size);
         cudaMalloc((void **) &h_JA, sizeof(int)* JA_size);
 
-        cudaMemcpy(h_b, b_mat, sizeof(int)*n*k);
-        cudaMemcpy(h_A, A, sizeof(int)*A_size);
-        cudaMemcpy(h_IA, IA, sizeof(int)*IA_size);
-        cudaMemcpy(h_JA, JA, sizeof(int)*JA_size);
+        cudaMemcpy(h_b, b_mat, sizeof(int)*n*k, cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_A, A, sizeof(int)*A_size, cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_IA, IA, sizeof(int)*IA_size, cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_JA, JA, sizeof(int)*JA_size, cudaMemcpyDeviceToHost);
         
         float gpu_elapsed_time_ms, cpu_elapsed_time_ms;
 
@@ -299,9 +339,6 @@ int main(int argc, char const *argv[])
         cudaEvent_t start, stop;
         cudaEventCreate(&start);
         cudaEventCreate(&stop);
-
-        
-
 
         unsigned int grid_rows = (m + BLOCK_SIZE - 1) / BLOCK_SIZE;
         unsigned int grid_cols = (k + BLOCK_SIZE - 1) / BLOCK_SIZE;
@@ -318,7 +355,6 @@ int main(int argc, char const *argv[])
             // start to count execution time of GPU version
             cudaEventRecord(start, 0);
             gpu_matrix_mult<<<dimGrid, dimBlock>>>(m, n, k,  A_size, IA_size, JA_size, h_A, h_IA, h_JA, h_b, h_c);  
-            cout << "mul ok" << endl;  
             cudaEventRecord(stop, 0);
         }
         // Transefr results from device to host 
@@ -327,16 +363,21 @@ int main(int argc, char const *argv[])
         // time counting terminate
         cudaEventSynchronize(stop);
 
+        cout << "gpu_ver" << endl;
+        for(int a = 0; a < m * k; a++){
+            cout << h_cc[a] << ' ' << endl;
+        }
+
         // compute time elapse on GPU computing
         cudaEventElapsedTime(&gpu_elapsed_time_ms, start, stop);
         printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on GPU: %f ms.\n", m, n, n, k, gpu_elapsed_time_ms);
 
-        cpu version
-
+        // cpu version
         // start the CPU version
+        int all_ok = 1;
         cudaEventRecord(start, 0);
 
-        cpu_matrix_mult(h_a, h_b, h_cc, m, n, k);
+        all_ok = matrix_multiply_cpu(m, n, k, A_size, IA_size, JA_size, A, IA, JA, b_mat, h_cc);
 
         cudaEventRecord(stop, 0);
         cudaEventSynchronize(stop);
@@ -344,19 +385,18 @@ int main(int argc, char const *argv[])
         printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on CPU: %f ms.\n", m, n, n, k, cpu_elapsed_time_ms);
 
         // validate results computed by GPU
-        int all_ok = 1;
-        for (int i = 0; i < m; ++i)
-        {
-            for (int j = 0; j < k; ++j)
-            {
-                //printf("[%d][%d]:%d == [%d][%d]:%d, ", i, j, h_cc[i*k + j], i, j, h_c[i*k + j]);
-                if(h_cc[i*k + j] != h_c[i*k + j])
-                {
-                    all_ok = 0;
-                }
-            }
-            //printf("\n");
-        }
+        // for (int i = 0; i < m; ++i)
+        // {
+        //     for (int j = 0; j < k; ++j)
+        //     {
+        //         //printf("[%d][%d]:%d == [%d][%d]:%d, ", i, j, d_cc[i*k + j], i, j, d_c[i*k + j]);
+        //         if(d_cc[i*k + j] != h_cc[i*k + j])
+        //         {
+        //             all_ok = 0;
+        //         }
+        //     }
+        //     //printf("\n");
+        // }
 
         // roughly compute speedup
         if(all_ok)
