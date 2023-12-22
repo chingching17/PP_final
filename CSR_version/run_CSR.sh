f6df48c2cd085fa3795577515d08c64d9c77a0b3
chingching17 <alicia.cs08@nctu.edu.tw>
2023-12-22 14:18:31 +0800

    add .sh

---
+++ CSR_version/run_CSR.sh
@@ -0,0 +1,5 @@
+./serial_CSR < matrix_csr.txt
+./Pthread_CSR < matrix_csr.txt
+./OpenMP_CSR < matrix_csr.txt
+./MPI_CSR < matrix_csr.txt
+./CUDA_CSR < matrix_csr.txt
---
+++ non_CSR/run_serial.sh
@@ -0,0 +1,5 @@
+./serial < matrix_data.txt
+./Pthread < matrix_data.txt
+./OpenMP < matrix_data.txt
+./MPI < matrix_data.txt
+./CUDA < matrix_data.txt
